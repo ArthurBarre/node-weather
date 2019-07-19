bcdc48612b13f2a1f1c33b8d9b3d572ada1a9219
ArthurBArre <arthur.barre@hetic.net>
2019-07-19 15:21:19 +0200

    fix problems

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request')
+const request = require('request');
 const app = express()
 
-app.set('view engine', 'ejs')
+const apiKey = 'ec86d806d03ce226c6d1db99c257c1ac'
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index', { weather: null, error: null });
 })
+
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let apiKey = `ec86d806d03ce226c6d1db99c257c1ac`
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-  request(url, function (err, respons, body) {
+  request(url, function (err, response, body) {
     if (err) {
       res.render('index', {
-        weather: null, error: 'ERREUR, la ville n\'a pas été trouvé'
-      })
+        weather: null, error: 'ERREUR, réessayer svp'
+      });
     } else {
-      let weather = JSON.parse(body);
+      let weather = JSON.parse(body)
       if (weather.main == undefined) {
         res.render('index', {
-          weather: null, error: 'ERREUR, la ville n\'a pas été trouvé'
+          weather: null, error: 'ERREUR, réessayer svp'
         });
       } else {
-        let weatherText = `Il fait ${weather.main.temp} degrés à ${weather.name}!`;
-        res.render('index', { weatherText: weatherText, error: null })
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {
+          weather: weatherText, error: null
+        });
       }
     }
-  })
+  });
 })
+
 app.listen(3000, function () {
-  console.log('App listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 })
